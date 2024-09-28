25a5984aa43c5b880db337d16e46efac90e0fda2
Aseem4091 <https://github.com/sagun03/furnishFantasy-ui.git>
2024-09-28 13:15:23 -0400

    The changes i have made to the new react project of DEVOPS

---
+++ src/AseemComponent1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AseemComponent1() {
+  return (
+    <div><h2>
+        My name is aseem and I have created new react component
+        </h2></div>
+  )
+}
+
+export default AseemComponent1
